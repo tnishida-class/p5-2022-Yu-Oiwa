bc26e045c974994f3622a9bb527f94cc730192b3
Yu-Oiwa <114969557+Yu-Oiwa@users.noreply.github.com>
2022-10-18 13:56:46 +0900

    Change section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,11 +1,17 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let vx;
+let x;
+let size;
+let g;
+const vxMax = 2
 
 function setup(){
   createCanvas(200, 200);
   count = 0;
-  cycle = 100;
+  cycle = 200;
+  g = 0.5
   vx = 1
 }
 
@@ -13,6 +19,17 @@ function draw(){
   background(160, 192, 255);
   count = (count + 1) % cycle;
   // BLANK[1]
-  size = count + vx
+  if(keyIsDown("A".charCodeAt(0))){
+    vx = vx * 2 
+    }
   ellipse(width / 2, height / 2, size);
+  count +=vx
+  vx += g
+  vx = constrain(vx, -vxMax, vxMax)
+  if(count < 100){
+    size = count
+    }
+  if(count > 100){
+    size = 200 - count
+  }   
 }
