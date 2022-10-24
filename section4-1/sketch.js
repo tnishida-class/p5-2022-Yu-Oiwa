adc2f92b6e0c386f5989c95117ea9da1466f08a7
Yu-Oiwa <114969557+Yu-Oiwa@users.noreply.github.com>
2022-10-24 15:12:28 +0900

    Change section4-1

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -19,15 +19,23 @@ function setup(){
 
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
+  average = sum / scores.length;
+  console.log(average);
   // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
 
   largest = 0;
   for(let i = 0; i < scores.length; i++){
+    if(scores[i] > largest){
+      largest = scores[i]
+    }
     // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
   }
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
+    if(scores[i] < smallest){
+      smallest = scores[i]
+    }
     // BLANK[3]　ヒント：最小値とだいたい同じ
   }
 
@@ -40,11 +48,20 @@ function setup(){
   for(let i = 0; i < scores.length; i++){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
+    if(scores[i] = largest){
+      fill(255, 0, 0)
+    }
+    if(scores[i] = smallest){
+      fill(0, 0, 255)
+    }
     // BLANK[4] ヒント: 条件分岐を使って色を変更します
     rect(i * dx + 2, height - h, dx - 4, h);
     fill(0);
     text(scores[i].toPrecision(3), i * dx, height - h);
   }
 
+  if(scores[i] = average){
+    line(0, height - h, width, height - h)
+  }
   // BLANK[5] 平均点の線を引きます
 }
